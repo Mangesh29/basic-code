554647ddba067f63656d074f088816b5fb4d187f
Mangesh29 <bhosalemangesh260@gmail.com>
2023-08-08 21:52:12 +0530

    basic code of c

---
+++ A13.c
@@ -0,0 +1,20 @@
+// Program to print 5 to 1 numbers on screen.
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+       int  ino=5;
+    for (i=ino; i>=1;i--)
+    {
+        printf("%d",i);
+
+    }
+
+}
+int main()
+{
+    Display();
+    return 0;
+    
+}
